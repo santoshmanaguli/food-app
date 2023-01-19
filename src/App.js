65bc4fd59cf544e0f38439cd3073330f59397f73
Santosh Managuli <rocksantosh15@gmail.com>
2023-01-19 23:07:54 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+function App() {
+  return (
+    <>
+      <div>
+        <h2 style={darkMode}>Hello World</h2>
+      </div>
+    </>
+  );
+}
+
+const darkMode = {
+  color: 'white'
+}
+
+export default App;
